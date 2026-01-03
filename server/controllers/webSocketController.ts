e65784c6346371937a50ba35b0c2f9c7a3afd0ec
Patrick <h.ilya2003@mail.ru>
2026-01-03 17:39:34 +0300

    refactor: remove websocket logic to websocket controller

---
+++ server/controllers/webSocketController.ts
@@ -0,0 +1,39 @@
+import type { UserRequest } from "../../shared/types";
+import type { ExtendedWebSocket } from "../types/types";
+
+export class WebSocketController {
+  private aWss: any;
+
+  constructor(aWss: any) {
+    this.aWss = aWss;
+  }
+
+  messageHandler(ws: any, rawMsg: string) {
+    try {
+      const msg: UserRequest = JSON.parse(rawMsg);
+      switch (msg.method) {
+        case "connection":
+          this.connectionHandler(ws, msg);
+          break;
+        case "draw":
+          this.broadcastConnection(ws, msg);
+          break;
+      }
+    } catch (e) {
+      console.log("Ошибка парсинга", e);
+    }
+  }
+
+  private connectionHandler = (ws: ExtendedWebSocket, msg: UserRequest) => {
+    ws.id = msg.id;
+    this.broadcastConnection(ws, msg);
+  };
+
+  private broadcastConnection = (ws: ExtendedWebSocket, msg: UserRequest) => {
+    this.aWss.clients.forEach((client: any) => {
+      if (client.id === msg.id) {
+        client.send(JSON.stringify(msg));
+      }
+    });
+  };
+}
--- server/index.ts
+++ server/index.ts
@@ -1,56 +1,27 @@
-import type { UserRequest } from "../shared/types";
-import type { Request, Response } from "express";
-import type { WebSocket } from "ws";
+import type { Request } from "express";
+import type { ExtendedWebSocket } from "./types/types";
 
 import express from "express";
-
 import cors from "cors";
 import expressWs from "express-ws";
 import drawingRouter from "./routes/drawingRouter";
+import { WebSocketController } from "./controllers/webSocketController";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 const WSServer = expressWs(app);
 const aWss = WSServer.getWss();
-
-interface ExtendedWebSocket extends WebSocket {
-  id: string;
-}
+const wsController = new WebSocketController(aWss);
 
 app.use(cors());
 app.use(express.json());
 
 (app as any).ws("/", (ws: ExtendedWebSocket, req: Request) => {
   ws.on("message", (rawMsg: string) => {
-    try {
-      const msg: UserRequest = JSON.parse(rawMsg);
-      switch (msg.method) {
-        case "connection":
-          connectionHandler(ws, msg);
-          break;
-        case "draw":
-          broadcastConnection(ws, msg);
-          break;
-      }
-    } catch (e) {
-      console.log("Ошибка парсинга", e);
-    }
+    wsController.messageHandler(ws, rawMsg);
   });
 });
 
 app.use("/", drawingRouter);
 
 app.listen(PORT, () => console.log(PORT + " is working"));
-
-const connectionHandler = (ws: ExtendedWebSocket, msg: UserRequest) => {
-  ws.id = msg.id;
-  broadcastConnection(ws, msg);
-};
-
-const broadcastConnection = (ws: ExtendedWebSocket, msg: UserRequest) => {
-  aWss.clients.forEach((client: any) => {
-    if (client.id === msg.id) {
-      client.send(JSON.stringify(msg));
-    }
-  });
-};
